247574e84918d7523d443c3164562395fb178f3e
Michael Bardsley <me@mic-b.co.uk>
2015-05-21 21:29:10 +0100

    added ex5.c

---
+++ ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Wow you can write comments in C... */
+int main (int argc, char *argv[])
+{
+    int distance = 353;
+
+    //this is also a another wonderful comment
+    printf("You are %d miles away from home.\n", distance);
+
+    return 0;
+}
